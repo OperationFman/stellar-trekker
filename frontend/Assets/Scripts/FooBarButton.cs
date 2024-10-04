78fa76db6feeca9b74804b17acd1666420fc1162
Franklin Von Moon <franklin.m.moon@gmail.com>
2024-10-04 10:23:25 +0700

    🚧 Init project

---
+++ frontend/Assets/Scripts/FooBarButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FooBarButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void ButtonCall()
+    {
+        // Add your button click logic here
+        Debug.Log("Button clicked!");
+    }
+
+}
